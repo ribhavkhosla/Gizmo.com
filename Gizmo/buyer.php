0750c1bd8e36cd0b3653159e5e49a96323a4019b
Aditya Nair <59489802+adityanaiir@users.noreply.github.com>
2022-03-03 15:44:07 -0500

    Update buyer.php

--- Gizmo/buyer.php
+++ Gizmo/buyer.php
@@ -9,7 +9,6 @@
     <title>Gizmo.com/ProductsPage</title>
 
     <link rel="stylesheet" href="buyer.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
