ff0132fc7f15152f6bdcee12a7ce45a56ad75995
ribhavkhosla <ribhavkhosla8968@icloud.com>
2022-03-25 18:37:09 -0400

    updates

---
+++ public/html_php/autofill.php
@@ -0,0 +1,28 @@
+
+
+<script>window.onload = function(){
+    auto()
+};
+</script>
+
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js%22%3E"></script>
+<link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500%22%3E">
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=AIzaSyC95Ub1JLB7GzPwnfg_90nOb3CfnSOOUIE"></script>
+<script>
+    var autofill;
+    function auto() {
+        autofill = new google.maps.places.Autocomplete(
+            (document.getElementById('autofill')), {
+                types: ['geocode'],
+                componentRestrictions: {
+                    country: "ca"
+                }
+            });
+        google.maps.event.addListener(autofill, 'place_changed', function() {
+
+        });
+    }
+</script>
+
--- public/html_php/postad.php
+++ public/html_php/postad.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <?php include('header.php')?>
+    <?php include('autofill.php')?>
     <title>Gizmo.com/PostAd</title>
     <link rel="stylesheet" href="/project-gizmo/public/css/postad.css">
 </head>
@@ -42,10 +43,15 @@
                 <input type="text" placeholder="Enter Description" name="Description" required>
 
                 <label for="location"><b>Location</b></label>
-                <input type="text" placeholder="Enter Location" name="location" required>
+                <input id="autofill" type="text" placeholder="Enter Location" name="location" required>
 
                 <label for="contact"><b>Contact Information</b></label>
                 <input type="text" placeholder="Enter Contact Information" name="contact" required>
+
+                <form action="upload.php" method="POST" enctype="multipart.form-data">
+                    <input type="file" name="file">
+                    <button type="submit" name="submit">UPLOAD IMAGE</button>
+                </form>
 
                 <input type="button" value="Previous">
                 <input type="submit" name="ad" value="Post Ad">
